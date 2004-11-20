edb578a5986e92d80468dcbad96f4e311e320907
gregsharp <>
2004-11-20 03:52:17 +0000

    Windows changes for 1.61.1.

--- makedist_win.bat
+++ makedist_win.bat
@@ -1,11 +1,13 @@
 set ver=1.61.1
-set bdir=d:\streamripper-%ver%
+set bdir=d:\streamripper-win32-%ver%
+deltree /y %bdir%
 mkdir %bdir%
 copy README %bdir%
 copy CHANGES %bdir%
 copy COPYING %bdir%
 copy THANKS %bdir%
-copy iconv-win32\dll\*.dll %bdir%
+@rem copy iconv-win32\dll\*.dll %bdir%
 copy tre-0.7.0\win32\Release\*.dll %bdir%
 copy consolewin32\release\sripper.exe %bdir%\streamripper.exe
-zip -9 -r \streamripper-%ver%.zip %bdir%
+zip -9 -r \streamripper-win32-%ver%.zip %bdir%
+deltree /y %bdir%
