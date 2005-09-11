c431e3f3471de185fcf43e92c604c33579ed2e2b
gregsharp <>
2005-09-11 16:50:21 +0000

    Remove obsolete files.  These have been merged into ripstream.

--- lib/ripshout.h
+++
@@ -1,9 +0,0 @@
-#ifndef __RIPSHOUT_H__
-#define __RIPSHOUT_H__
-
-#include "srtypes.h"
-
-error_code ripshout_init(IO_DATA_INPUT *in, IO_GET_STREAM *getstream, int meta_interval);
-void ripshout_destroy();
-
-#endif //__RIPSHOUT_H__
