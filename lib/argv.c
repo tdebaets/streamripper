9ae2c8a7745acdcf2d4ed295f4b7b7155b02349c
gregsharp <>
2005-11-12 06:19:18 +0000

    Update argv.c for portability

--- lib/argv.c
+++ lib/argv.c
@@ -22,8 +22,12 @@ Boston, MA 02111-1307, USA.  */
 /*  Create and destroy argument vectors.  An argument vector is simply an
     array of string pointers, terminated by a NULL pointer. */
 
+#if defined (commentout)
 #include "ansidecl.h"
 #include "libiberty.h"
+#endif
+void freeargv (char **vector);
+
 
 #define ISBLANK(ch) ((ch) == ' ' || (ch) == '\t')
 
