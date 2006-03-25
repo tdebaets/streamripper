956024b8f4ffdb1a278adf288b3b6c077f085602
gregsharp <>
2006-03-25 23:57:20 +0000

    Update windows packaging script for TRE

--- makedist_win.bat
+++ makedist_win.bat
@@ -12,7 +12,7 @@ copy fetch_external_metadata.pl %bdir%
 copy libogg-1.1.3\*.dll %bdir%
 copy libvorbis-1.1.2\*.dll %bdir%
 @rem copy iconv-win32\dll\*.dll %bdir%
-copy tre-0.7.0\win32\Release\*.dll %bdir%
+copy tre-0.7.2\win32\Release\*.dll %bdir%
 copy consolewin32\release\sripper.exe %bdir%\streamripper.exe
 erase \streamripper-win32-%ver%.zip
 zip -9 -r \streamripper-win32-%ver%.zip %bdir%
