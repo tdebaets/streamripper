b667e9b9490c603469611dadfa861628b08d1cca
gregsharp <>
2007-01-14 19:58:21 +0000

    Fix autoconf tests for regular expressions library

--- config.sh
+++ config.sh
@@ -3,19 +3,19 @@ automake --add-missing --foreign Makefile
 autoconf
 autoheader
 
-cd libmad-0.15.1b
-aclocal
-automake --add-missing --foreign Makefile
-autoconf
-autoheader
-cd ..
+# cd libmad-0.15.1b
+# aclocal
+# automake --add-missing --foreign Makefile
+# autoconf
+# autoheader
+# cd ..
 
-cd tre-0.7.2
-aclocal -I m4
-automake --add-missing --foreign Makefile
-autoconf
-autoheader
-cd ..
+# cd tre-0.7.2
+# aclocal -I m4
+# automake --add-missing --foreign Makefile
+# autoconf
+# autoheader
+# cd ..
 
 rm -f config.cache
 ./configure --with-included-libmad --with-included-tre --with-included-argv
