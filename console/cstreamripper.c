129aacbbacb305ffb1e0fe865205a1c54d484121
gregsharp <>
2007-07-07 20:07:42 +0000

    Updates for win32 curses

--- console/cstreamripper.c
+++ console/cstreamripper.c
@@ -1,17 +1,26 @@
 /* cstreamripper.c
  * Curses front end for streamripper
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
  */
 #include "srconfig.h"
 #include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
-#if defined (WIN32)
-#include <cdk.h>
-#else
-#include <cdk/cdk.h>
-#endif
+#include "sr_cdk.h"
 
 /* GCS FIX: need to set HAVE_XCURSES as needed */
 #ifdef HAVE_XCURSES
