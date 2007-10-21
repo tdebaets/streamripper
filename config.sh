277eea7173c44c11c70b633cd987674b1d413729
gregsharp <>
2007-10-21 03:40:58 +0000

    Remove build of cstreamripper for release

--- config.sh
+++ config.sh
@@ -3,12 +3,12 @@ automake --add-missing --foreign Makefile
 autoconf
 autoheader
 
-# cd libmad-0.15.1b
-# aclocal
-# automake --add-missing --foreign Makefile
-# autoconf
-# autoheader
-# cd ..
+cd libmad-0.15.1b
+aclocal
+automake --add-missing --foreign Makefile
+autoconf
+autoheader
+cd ..
 
 cd tre-0.7.2
 aclocal -I m4
