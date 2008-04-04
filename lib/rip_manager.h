4c1e7887510ae905aab01892c0be5f245a53a5e1
gregsharp <>
2008-04-04 03:18:47 +0000

    Upgrade version

--- lib/rip_manager.h
+++ lib/rip_manager.h
@@ -23,7 +23,7 @@
 #include "errors.h"
 #include "compat.h"
 
-#define SRVERSION	"1.63-beta-5"
+#define SRVERSION	"1.63-beta-6"
 
 #if defined (WIN32)
 #define SRPLATFORM      "windows"
--- makedist_win.bat
+++ makedist_win.bat
@@ -1,4 +1,4 @@
-set ver=1.63-beta-5
+set ver=1.63-beta-6
 set bdir=d:\streamripper-win32-%ver%
 deltree /y %bdir%
 mkdir %bdir%
