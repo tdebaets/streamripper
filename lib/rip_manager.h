cfae41197fca0c7a3dd405a711809ce56d404fc7
gregsharp <>
2008-04-27 16:13:29 +0000

    Upgrade version number, update man page

--- lib/rip_manager.h
+++ lib/rip_manager.h
@@ -23,7 +23,7 @@
 #include "errors.h"
 #include "compat.h"
 
-#define SRVERSION	"1.63-beta-7"
+#define SRVERSION	"1.63-beta-8"
 
 #if defined (WIN32)
 #define SRPLATFORM      "windows"
--- makedist_win.bat
+++ makedist_win.bat
@@ -1,4 +1,4 @@
-set ver=1.63-beta-7
+set ver=1.63-beta-8
 set bdir=d:\streamripper-win32-%ver%
 deltree /y %bdir%
 mkdir %bdir%
