036a17508e2b8e8b93a1eec1ea96d2f278d5a4c6
gregsharp <>
2008-12-22 14:57:49 +0000

    Remove automatically generated configure files

--- config.sh
+++ config.sh
@@ -4,12 +4,12 @@ automake --foreign lib/Makefile
 autoconf
 autoheader
 
-# cd libmad-0.15.1b
-# aclocal
-# automake --add-missing --foreign Makefile
-# autoconf
-# autoheader
-# cd ..
+cd libmad-0.15.1b
+aclocal
+automake --add-missing --foreign Makefile
+autoconf
+autoheader
+cd ..
 
 # cd tre-0.7.2
 # aclocal -I m4
